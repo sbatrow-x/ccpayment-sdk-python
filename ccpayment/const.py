19316283085801293ecb536720b1dcc55707e7c7
sbatrow-x <84258010+sbatrow-x@users.noreply.github.com>
2024-04-04 13:23:11 +0530

    Update const.py

--- ccpayment/const.py
+++ ccpayment/const.py
@@ -2,44 +2,44 @@
 HOST = "https://admin.ccpayment.com"  # produce
 
 # create order
-CREATE_ORDER_URL = HOST + "/ccpayment/v1/bill/create"
+CREATE_ORDER_URL = HOST + "/ccpayment/v2/bill/create"
 
 # checkout url
-CHECKOUT_URL = HOST + "/ccpayment/v1/concise/url/get"
+CHECKOUT_URL = HOST + "/ccpayment/v2/concise/url/get"
 
 # support coin all
-SUPPORT_COIN_URL = HOST + "/ccpayment/v1/coin/all"
+SUPPORT_COIN_URL = HOST + "/ccpayment/v2/coin/all"
 
 
 # support token
-SUPPORT_TOKEN_URL = HOST + "/ccpayment/v1/support/token"
+SUPPORT_TOKEN_URL = HOST + "/ccpayment/v2/support/token"
 
 # get token chains
-TOKEN_CHAIN_URL = HOST + "/ccpayment/v1/token/chain"
+TOKEN_CHAIN_URL = HOST + "/ccpayment/v2/token/chain"
 
 # get token rate
-TOKEN_RATE_URL = HOST + "/ccpayment/v1/token/rate"
+TOKEN_RATE_URL = HOST + "/ccpayment/v2/token/rate"
 
 # api - withdrawal
-WITHDRAW_API_URL = HOST + "/ccpayment/v1/withdraw"
+WITHDRAW_API_URL = HOST + "/ccpayment/v2/withdraw"
 
 # c user check
-CHECK_USER_URL = HOST + "/ccpayment/v1/check/user"
+CHECK_USER_URL = HOST + "/ccpayment/v2/check/user"
 
 # get token assets
-ASSETS_URL = HOST + "/ccpayment/v1/assets"
+ASSETS_URL = HOST + "/ccpayment/v2/assets"
 
 # get network fee
-NETWORK_FEE_URL = HOST + "/ccpayment/v1/network/fee"
+NETWORK_FEE_URL = HOST + "/ccpayment/v2/network/fee"
 
 # get order info
-API_ORDER_INFO_URL = HOST + "/ccpayment/v1/bill/info"
+API_ORDER_INFO_URL = HOST + "/ccpayment/v2/bill/info"
 
 # get payment address
-PAYMENT_ADDRESS_URL = HOST + "/ccpayment/v1/payment/address/get"
+PAYMENT_ADDRESS_URL = HOST + "/ccpayment/v2/payment/address/get"
 
 # get chain height info
-CHAIN_HEIGHT_INFO_URL = HOST + "/ccpayment/v1/get/network/height/info"
+CHAIN_HEIGHT_INFO_URL = HOST + "/ccpayment/v2/get/network/height/info"
 
 # header key
 # app id
